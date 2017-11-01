7ac7235e83ad51ac9450f0508d0342e486b90f36
Andrew Polk <polk@umail.ucsb.edu>
2017-11-01 14:19:09 -0700

    finished switching ojects for lab04

--- src/edu/ucsb/cs56/drawings/drew_polk/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/drawings/drew_polk/advanced/AllMyDrawings.java
@@ -14,30 +14,30 @@ import edu.ucsb.cs56.drawings.utilities.GeneralPathWrapper;
  * 
  * @author Phill Conrad
  * @author Andrew Polk
- * @version for UCSB CS56, W16 
+ * @version for UCSB CS56, F17 
  */
 
 public class AllMyDrawings
 {
-    /** Draw a picture with a few houses 
+    /** Draw a picture with a few lightbulbs and lamps 
      */
     
     public static void drawPicture1(Graphics2D g2) {
 	
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
+	Lightbulb bulb1 = new Lightbulb(100,250,50,75);
+	g2.setColor(Color.CYAN); g2.draw(bulb1);
 	
-	// Make a black house that's half the size, 
+	// Make a black lightbulb that's half the size, 
 	// and moved over 150 pixels in x direction
 	
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+	Shape bulb2 = ShapeTransforms.scaledCopyOfLL(bulb1,0.5,0.5);
+	bulb2 = ShapeTransforms.translatedCopyOf(bulb2,150,0);
+	g2.setColor(Color.BLACK); g2.draw(bulb2);
 	
-	// Here's a house that's 4x as big (2x the original)
+	// Here's a lightbulb that's 4x as big (2x the original)
 	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+	bulb2 = ShapeTransforms.scaledCopyOfLL(bulb2,4,4);
+	bulb2 = ShapeTransforms.translatedCopyOf(bulb2,150,0);
 	
 	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
@@ -49,53 +49,56 @@ public class AllMyDrawings
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
+	g2.draw(bulb2); 
 	
-	// Draw two houses with Windows
+	// Draw two lamps
 	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+	Lamp lamp1 = new Lamp(50,350,40,75);
+	Lamp lamp2 = new Lamp(200,350,200,100);
+	Shape lamp3 = ShapeTransforms.scaledCopyOfLL(lamp2, 0.75, 0.75);
+	lamp3 = ShapeTransforms.translatedCopyOf(lamp3, -100, -150);
 	
-	g2.draw(hw1);
-	g2.setColor(new Color(0x8F00FF)); g2.draw(hw2);
+	g2.draw(lamp1);
+	g2.setColor(new Color(0x8F00FF)); g2.draw(lamp2);
+	g2.setColor(new Color(0x8F157D)); g2.draw(lamp3);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A few houses by Andrew Polk", 20,20);
+	g2.drawString("A few lightbulbs and lamps by Andrew Polk", 20,20);
     }
     
     
-    /** Draw a picture with a few houses and coffee cups
+    /** Draw a picture with a few lightbulbs and lamps
      */
     public static void drawPicture2(Graphics2D g2) {
 	
-	// Draw some coffee cups.
+	// Draw some lightbulbs.
 	
-	CoffeeCup large = new CoffeeCup(100,50,225,150);
-	CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
-	CoffeeCup tallSkinny = new CoffeeCup(20,150,20,40);
-	CoffeeCup shortFat = new CoffeeCup(20,250,40,20);
+	Lamp large = new Lamp(100,50,225,150);
+	Lamp small = new Lamp(20,50,40,30);
+	Lamp tallSkinny = new Lamp(20,150,20,40);
+	Lamp shortFat = new Lamp(20,250,40,20);
 	
 	g2.setColor(Color.RED);     g2.draw(large);
-	g2.setColor(Color.GREEN);   g2.draw(smallCC);
+	g2.setColor(Color.GREEN);   g2.draw(small);
 	g2.setColor(Color.BLUE);    g2.draw(tallSkinny);
 	g2.setColor(Color.MAGENTA); g2.draw(shortFat);
 	
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
+	Lightbulb bulb1 = new Lightbulb(100,250,50,75);
+	g2.setColor(Color.CYAN); g2.draw(bulb1);
 	
-	// Make a black house that's half the size, 
+	// Make a black lightbulb that's half the size, 
 	// and moved over 150 pixels in x direction
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+	Shape bulb2 = ShapeTransforms.scaledCopyOfLL(bulb1,0.5,0.5);
+	bulb2 = ShapeTransforms.translatedCopyOf(bulb2,150,0);
+	g2.setColor(Color.BLACK); g2.draw(bulb2);
 	
-	// Here's a house that's 4x as big (2x the original)
+	// Here's a lightbulb that's 4x as big (2x the original)
 	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+	bulb2 = ShapeTransforms.scaledCopyOfLL(bulb2,4,4);
+	bulb2 = ShapeTransforms.translatedCopyOf(bulb2,150,0);
 	
 	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
@@ -107,45 +110,49 @@ public class AllMyDrawings
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
+	g2.draw(bulb2); 
 	
-	// Draw two houses with Windows
+	// Draw two lamps
 	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+	Lamp lamp1 = new Lamp(50,350,40,75);
+	Lamp lamp2 = new Lamp(200,350,200,100);
 	
-	g2.draw(hw1);
+	g2.draw(lamp1);
 	g2.setColor(new Color(0x8F00FF)); 
 	
-	// Rotate the second house 45 degrees around its center.
-	Shape hw3 = ShapeTransforms.rotatedCopyOf(hw2, Math.PI/4.0);
+	// Rotate the second lamp 45 degrees around its center.
+	Shape lamp3 = ShapeTransforms.rotatedCopyOf(lamp2, Math.PI/4.0);
 	
-	g2.draw(hw3);
+	g2.draw(lamp3);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A bunch of Coffee Cups and a few houses by Andrew Polk", 20,20);
+	g2.drawString("A bunch of Lightbulbs and a few lamps by Andrew Polk", 20,20);
     }
     
-    /** Draw a different picture with a few houses and coffee cups
+    /** Draw a different picture with a few lamps
      */
     
     public static void drawPicture3(Graphics2D g2) {
 	
 	// label the drawing
 	
-	g2.drawString("A bunch of Coffee Cups by Andrew Polk", 20,20);
+	g2.drawString("A bunch of lamps by Andrew Polk", 20,20);
 	
 	
-	// Draw some coffee cups.
+	// Draw some lamps.
 	
-	CoffeeCup large = new CoffeeCup(100,50,225,150);
-	CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
+	Lamp large = new Lamp(100,50,225,150);
+	Lamp small = new Lamp(20,50,40,30);
+	Shape lamp3 = ShapeTransforms.scaledCopyOfLL(small, 1, 1.5);
+	lamp3 = ShapeTransforms.translatedCopyOf(lamp3, 50, 50);
+	lamp3 = ShapeTransforms.rotatedCopyOf(lamp3, Math.PI/2.0);
 	
 	g2.setColor(Color.RED);     g2.draw(large);
-	g2.setColor(Color.GREEN);   g2.draw(smallCC);
+	g2.setColor(Color.GREEN);   g2.draw(small);
+	g2.setColor(Color.BLUE); g2.draw(lamp3);
 	
     }       
 }
